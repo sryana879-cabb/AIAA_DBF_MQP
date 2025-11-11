62824438b0bc4788e9d34e2448041f6b0f274e52
Ryan Smith <sryana879@gmail.com>
2025-11-11 10:50:35 -0500

    k

--- RC_ClassIIMethod.m
+++ RC_ClassIIMethod.m
@@ -1,22 +1,9 @@
 % RC Class II Method
 clc; close all; clear all;
 
-%Given parameters
-balsa_rho = 5; %lbs/ft^3
-basswood_rho = 25; %lbs/ft^3
-foam_rho = 3; 
-
-%Wing Weight
-c_wing = 0.821; %ft, MAC from Ch4
-CG_wing = 0.4*c; %ft, assume CG is 40% of Chord
-b_wing = 3; %ft, effective span
-S_wing = b_wing*c_wing; %SA of wing
-AR_wing = b_wing/c_wing;
-
-%Tail Weight Estimation
-asghasdgha
-ksfzncfseuidsui
-styuff
+%% Use Given Parameters to Estimate Weight
+[W_structure] = RC_Weight_Estimation_Method_II();
+fprintf('Total Structural Weight: %.4f lb\n', W_structure);
 
 
 
