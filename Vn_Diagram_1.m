ac330e5825cf2b4ed985dae33946d7a0dd9df1e5
rmmay <rmmay@130.215.125.214>
2025-11-15 14:33:46 -0500

    added more

--- Vn_Diagram_1.m
+++ Vn_Diagram_1.m
@@ -1,16 +1,17 @@
-% V-n
+% V-n diagram
+close all; clear all; clc;
 
 g = 9.81;
-c = 10/12;
+c = 7.08/12; % feet
 WS = 3.5;                 % lb/ft^2
-rho = 1.198 * 0.00194032; % slugs/ft^3
-Cl_max = 1.0;
+rho = 0.00230; % slugs/ft^3
+Cl_max = 1.3642;
 Cn_max = Cl_max*1.1;
-CL3 = 1.6;
+CL3 = 4.15;
 Kc = 33;                  
 Ude = 12;
 Ude2 = 6;                 % fps
-V = linspace(0,250, 100); 
+V = linspace(0,600, 100); 
 
 % Kg and mug
 mug = 2 * (WS / (rho * c * g * CL3));
@@ -19,6 +20,10 @@ Kg = 0.88 * (mug / (5.3 + mug));
 % nlims
 nlimpos = 3;
 nlimneg = -0.4 * nlimpos;
+
+% nults
+nultpos = 1.5*nlimpos;
+nultneg = 1.5*nlimneg;
 
 % Stall and Cruise
 Vstall = sqrt(2 * (WS / (rho * Cn_max)));
@@ -36,16 +41,24 @@ nlimgustCneg = 1 - ((Kg.*Ude.*V.*CL3)./(498 .* WS));
 nlimgustD = 1 + ((Kg.*Ude2.*V.*CL3)./(498 .* WS));
 nlimgustDneg = 1 - ((Kg.*Ude2.*V.*CL3)./(498 .* WS));
 
+
+
+
+
+
+
 % Plot 
 figure; hold on; grid on;
-plot(V, n, 'b', 'LineWidth', 1);
-plot(V, n_neg, 'b','LineWidth', 1);
+plot(V, n, 'c', 'LineWidth', 1);
+plot(V, n_neg, 'c','LineWidth', 1);
 yline(nlimpos, '--', 'LineWidth', 1);
 yline(nlimneg, '--', 'LineWidth', 1);
-plot(V, nlimgustC, '--r', 'LineWidth', 1);
-plot(V, nlimgustCneg, '--r', 'LineWidth', 1);
-plot(V, nlimgustD, '--g', 'LineWidth', 1);
-plot(V, nlimgustDneg, '--g', 'LineWidth', 1);
+yline(nultpos, '--m', 'LineWidth', 1);
+yline(nultneg, '--m', 'LineWidth', 1);
+plot(V, nlimgustC, '--g', 'LineWidth', 1);
+plot(V, nlimgustCneg, '--g', 'LineWidth', 1);
+plot(V, nlimgustD, '--b', 'LineWidth', 1);
+plot(V, nlimgustDneg, '--b', 'LineWidth', 1);
 plot(Vcruise_min, 0, 'yo', 'MarkerFaceColor','y');
 plot(Vstall, 0, 'ko', 'MarkerFaceColor', 'k');
 plot(Vdive_min, 0, 'bo', 'MarkerFaceColor', 'b');
@@ -54,3 +67,26 @@ title('V-n Diagram');
 xlabel('V (ft/s)');
 ylabel('n (Loading Factor)');
 ylim([-2 4]);
+text(V(end), n(end), ' Stall +', 'Color','b', 'HorizontalAlignment','right');
+text(V(end), n_neg(end), ' Stall -', 'Color','b', 'HorizontalAlignment','right');
+text(V(end), nlimgustC(end), ' Gust C +', 'Color','r', 'HorizontalAlignment','right');
+text(V(end), nlimgustCneg(end), ' Gust C -', 'Color','r', 'HorizontalAlignment','right');
+text(V(end), nlimgustD(end), ' Gust D +', 'Color','g', 'HorizontalAlignment','right');
+text(V(end), nlimgustDneg(end), ' Gust D -', 'Color','g', 'HorizontalAlignment','right');
+text(10, nlimpos, ' nlim positive', 'Color','k');
+text(10, nlimneg, ' nlim negagive', 'Color','k');
+text(10, nultpos, ' nult positive', 'Color','m');
+text(10, nultneg, ' nult negative', 'Color','m');
+
+
+diff2 = nlimgustCneg - nultneg;
+idx2 = find(diff2 .* circshift(diff2,-1) <= 0, 1);   % where signs change
+V_int2 = V(idx2);
+n_int2 = nlimgustCneg(idx2);
+xline(V_int2, 'm--', 'LineWidth', 1.5);
+
+diff3 = nlimgustDneg - nultneg;
+idx3 = find(diff3 .* circshift(diff3,-1) <= 0, 1);   % where signs change
+V_int3 = V(idx3);
+n_int3 = nlimgustDneg(idx3);
+xline(V_int3, 'm--', 'LineWidth', 1.5);
