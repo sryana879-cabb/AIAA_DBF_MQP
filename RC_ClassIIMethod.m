c3bb62762ae93f909082408557f5e2a2c34c6934
Ryan Smith <sryana879@gmail.com>
2025-11-22 09:45:46 -0500

    yep

--- RC_ClassIIMethod.m
+++ RC_ClassIIMethod.m
@@ -2,7 +2,7 @@
 clc; close all; clear all;
 
 %% Use Given Parameters to Estimate Weight
-[W_wing, W_HT, W_VT, W_fuselage, W_structure, W_booms, weight_details] = RC_Weight_Estimation_Method_II();
+[W_wing, W_HT, W_VT, W_fuselage, W_booms, W_structure, W_avionics, W_powerplant, W_total, weight_details, cg_details] = RC_Weight_Estimation_Method_II();
 fprintf('Total Structural Weight: %.4f lb\n', W_structure);
 fprintf('Wing Weight: %.4f lb\n', W_wing);
 fprintf('HT Weight: %.4f lb\n', W_HT);
